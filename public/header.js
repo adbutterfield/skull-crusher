57cacd3541a8ca32ba4fd47244e91218b82816a0
Adam Butterfield <adb707@yahoo.com>
2019-03-16 16:14:01 +0900

    Add header component

---
+++ public/header.js
@@ -0,0 +1,24 @@
+/**
+ * Class for header component
+ */
+
+import config from './config.js';
+
+export default class Header {
+  constructor(canvasWidth, height) {
+    this.xPos = 0;
+    this.yPos = 0;
+    this.width = canvasWidth;
+    this.height = height;
+  }
+
+  draw(ctx, canvasWidth) {
+    ctx.fillStyle = config.backgroundColor;
+    ctx.fillRect(this.xPos, this.yPos, this.width, this.height);
+
+    ctx.fillStyle = config.textColor;
+    ctx.moveTo(this.xPos, this.height);
+    ctx.lineTo(this.width, this.height);
+    ctx.stroke();
+  }
+}
