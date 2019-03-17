f84a08b4876c84b0fbb5c4be65bce6a5588f2b4c
Adam Butterfield <adb707@yahoo.com>
2019-03-17 10:49:19 +0900

    Add spider web to title screen

--- public/title-screen.js
+++ public/title-screen.js
@@ -12,18 +12,28 @@ export default class TitleScreen {
   load(ctx, isSmallScreen) {
     ctx.textAlign = 'center';
 
-    ctx.font = `${isSmallScreen ? '40px' : '60px'} ${config.font}`;
+    ctx.font = `${isSmallScreen ? '40px' : '80px'} Creepster`;
     ctx.fillStyle = config.textColor;
-    ctx.fillText('SKULL CRUSHER', this.centerX, `${isSmallScreen ? 100 : 150}`);
+    ctx.fillText('SKULL CRUSHER', this.centerX, isSmallScreen ? 100 : 200);
 
-    ctx.font = `${isSmallScreen ? '28px' : '40px'} ${config.font}`;
-    ctx.fillText('Start', this.centerX, `${isSmallScreen ? 200 : 400}`);
+    ctx.font = `${isSmallScreen ? '28px' : '34px'} ${config.font}`;
+    ctx.fillText('Start', this.centerX, isSmallScreen ? 200 : 400);
 
-    ctx.font = `${isSmallScreen ? '20px' : '30px'} ${config.font}`;
+    ctx.font = `${isSmallScreen ? '20px' : '26px'} ${config.font}`;
     ctx.fillText(
       'TAP or PRESS ENTER',
       this.centerX,
       `${isSmallScreen ? 240 : 440}`
+    );
+
+    let img = new Image();
+    img.src = '../assets/spider-web.svg';
+    ctx.drawImage(
+      img,
+      isSmallScreen ? -40 : -80,
+      isSmallScreen ? -36 : -72,
+      isSmallScreen ? 100 : 200,
+      isSmallScreen ? 100 : 200
     );
   }
 }
